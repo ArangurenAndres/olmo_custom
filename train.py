83b63ff83eb7b5204c390b1b5a0d97aeacb3eac4
Max van den Boom <maxvandenboom@Maxs-MacBook-Pro.local>
2025-05-27 12:32:27 +0200

    Save dir fix

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def main():
         timestamp = time.strftime('%m-%d_%H-%M')
         run_dir = os.path.join(config["data_dir"], "checkpoints", f"run_{timestamp}")
         # save_dir = os.path.join("/scratch-shared/tmp.GcVy0pChFL", f"checkpoints_{timestamp}")
-        save_dir = os.path.join("/train_run", f"checkpoints_{timestamp}")
+        save_dir = os.path.join("train_run", f"checkpoints_{timestamp}")
         work_dir = os.path.join(run_dir, "trainer_work_dir")
         os.makedirs(run_dir, exist_ok=True)
         os.makedirs(save_dir, exist_ok=True) 
